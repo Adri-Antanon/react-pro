bb9453d436b190fab33a81fa04c967066efb1d59
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-04-12 11:06:12 +0200

    feat: add simple validation

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,14 +1,61 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+
+  const { email, name, password1, password2 } = registerData;
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setRegisterData((prevValue) => ({
+      ...prevValue,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('submit', registerData);
+  };
+
   return (
     <div>
       <h1>RegisterPage</h1>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Repeat password" />
+      <form noValidate onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password1"
+          value={password1}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          name="password2"
+          placeholder="Repeat password"
+          value={password2}
+          onChange={onChange}
+        />
 
         <button type="submit">Register</button>
       </form>

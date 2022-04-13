0b4b2592a3c3df0af817a10245613cfe05d14861
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-04-13 15:49:29 +0200

    feat: add manual validations

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,10 +1,36 @@
-import { useFormik } from 'formik';
+import { FormikErrors, useFormik } from 'formik';
 
 import '../styles/styles.css';
 
-type Props = {};
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
-export const FormikBasicPage = (props: Props) => {
+export const FormikBasicPage = () => {
+  const validate = (values: FormValues) => {
+    const errors: FormikErrors<FormValues> = {};
+    if (!values.firstName) {
+      errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+      errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  };
+
   const { values, handleChange, handleSubmit } = useFormik({
     initialValues: {
       firstName: '',
@@ -14,6 +40,7 @@ export const FormikBasicPage = (props: Props) => {
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
+    validate,
   });
 
   return (

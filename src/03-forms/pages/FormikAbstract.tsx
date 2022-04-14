395b3af6113ab2024fa639487d28a3226c40efbe
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-04-14 16:18:19 +0200

    feat: add formik abstractation with useField

---
+++ src/03-forms/pages/FormikAbstract.tsx
@@ -0,0 +1,83 @@
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { MyTextInput } from '../components/MyTextInput';
+
+import '../styles/styles.css';
+
+export const FormikAbstract = () => {
+  return (
+    <div>
+      <h1>Formik Abstract Tutorial</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          terms: false,
+          jobType: '',
+        }}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values, null, 2));
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          lastName: Yup.string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+          terms: Yup.boolean().oneOf([true], 'You must accept the terms'),
+          jobType: Yup.string()
+            .notOneOf(['it-junior'], 'This option is not allowed')
+            .required('Required'),
+        })}
+      >
+        {(formik) => (
+          <Form>
+            <MyTextInput
+              label="First Name"
+              name="firstName"
+              placeholder="Adrián"
+              type="text"
+            />
+
+            <MyTextInput
+              label="Last Name"
+              name="lastName"
+              placeholder="Antañón"
+              type="text"
+            />
+
+            <MyTextInput
+              label="Email Address"
+              name="email"
+              placeholder="test@gmail.com"
+              type="email"
+            />
+
+            <label htmlFor="jobType">Job Type</label>
+            <Field name="jobType" as="select">
+              <option value={''}>Select a job type</option>
+              <option value="designer">Designer</option>
+              <option value="developer">Developer</option>
+              <option value="it-senior">IT Senior</option>
+              <option value="it-junior">IT Junior</option>
+            </Field>
+            <ErrorMessage component={'span'} name="jobType" />
+
+            <label>
+              <Field name="terms" type="checkbox" />
+              Terms and Conditions
+            </label>
+            <ErrorMessage component={'span'} name="terms" />
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,6 +6,7 @@ import logo from '../logo.svg';
 import { FormikBasicPage } from '../03-forms/pages/FormikBasicPage';
 import { FormikYupPage } from '../03-forms/pages/FormikYupPage';
 import { FormikComponents } from '../03-forms/pages/FormikComponents';
+import { FormikAbstract } from '../03-forms/pages/FormikAbstract';
 
 export const Navigation: React.FC = () => {
   return (
@@ -56,6 +57,14 @@ export const Navigation: React.FC = () => {
             </li>
             <li>
               <NavLink
+                to="/formik-abstract"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Formik Abstract
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
                 to="/about"
                 className={({ isActive }) => (isActive ? 'nav-active' : '')}
               >
@@ -78,6 +87,7 @@ export const Navigation: React.FC = () => {
           <Route path="formik-basic" element={<FormikBasicPage />} />
           <Route path="formik-yup" element={<FormikYupPage />} />
           <Route path="formik-components" element={<FormikComponents />} />
+          <Route path="formik-abstract" element={<FormikAbstract />} />
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
           <Route path="/*" element={<Navigate to="home" replace />} />

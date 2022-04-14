1dd8f1b6d33f16ead0a5d43c1f064cd6e286b654
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-04-14 17:10:16 +0200

    refactor: remove console logs & import pages from index

--- src/03-forms/components/MyTextInput.tsx
+++ src/03-forms/components/MyTextInput.tsx
@@ -1,23 +1,20 @@
-import { useField } from 'formik';
+import { ErrorMessage, useField } from 'formik';
 
 interface Props {
   label: string;
   name: string;
-  type?: 'text' | 'email' | 'number' | 'password';
+  type?: 'text' | 'email' | 'password';
   placeholder?: string;
   [x: string]: any;
 }
 
 export const MyTextInput = ({ label, ...props }: Props) => {
-  const [field, meta] = useField(props);
-  console.log(field, meta);
+  const [field] = useField(props);
   return (
     <>
       <label htmlFor={props.id || props.name}>{label}</label>
       <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error && (
-        <span className="error">{meta.error}</span>
-      )}
+      <ErrorMessage name={props.name} component="span" />
     </>
   );
 };
